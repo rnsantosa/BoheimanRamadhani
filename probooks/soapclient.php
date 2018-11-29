29fd3e1eb736975d472bd889a697a7097e44cd58
Rahmat Santosa <rnsantosa@yahoo.com>
2018-11-29 12:43:12 +0700

    fix error sosoapclient

--- probooks/soapclient.php
+++ probooks/soapclient.php
@@ -1,7 +1,6 @@
 <?php
   $client = new SoapClient("http://localhost:8888/service/transaksi?wsdl");
   $judul = $_POST['judul'];
-  $id = $_POST['id'];
   $params = array(
     "arg0" => $_POST['judul']
   );
