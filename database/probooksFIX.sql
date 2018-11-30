429d0a99e2f5c224ff04067b510ab64f172b6966
rakahadhyana <hadhyana@gmail.com>
2018-11-30 10:26:33 +0700

    fix database dump

--- database/probooksFIX.sql
+++ database/probooksFIX.sql
@@ -88,7 +88,7 @@ CREATE TABLE `session` (
   PRIMARY KEY (`session_id`),
   UNIQUE KEY `session_id_UNIQUE` (`session_id`),
   KEY `FK_session_user_idx` (`username`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
